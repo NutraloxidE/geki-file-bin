6cf08bfad70cce6bc62b03a3c85a30ba6c6f54a7
NutraloxidE <niconico.yokato@gmail.com>
2025-07-16 13:32:05 +0900

    added ecosystem config

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv"; // dotenvをインポート
+dotenv.config({ path: ".env.local" }); // .env.local を読み込む
+
+const ecosystemConfig = {
+  apps: [
+    {
+      name: "geki-file-bin", // アプリケーション名
+      script: "npm", // 実行するコマンド
+      args: "start", // npm start を実行
+      env: {
+        NODE_ENV: "production",
+        ADMIN_ID: process.env.ADMIN_ID, // .env.local から読み込む
+        JWT_SECRET: process.env.JWT_SECRET, // .env.local から読み込む
+        ADMIN_AUTHENTICATOR_SETUP_KEY: process.env.ADMIN_AUTHENTICATOR_SETUP_KEY, // .env.local から読み込む
+      },
+    },
+  ],
+};
+
+export default ecosystemConfig;
